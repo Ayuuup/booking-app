e5c1ac27f706589292f2963e61f1651bc2fa7e88
Ayuuup <babakhouyaayoub@gmail.com>
2025-06-14 18:52:46 +0100

    added a layout for the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-
+import Layout from './Layout'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import {Routes,Route} from 'react-router-dom'
@@ -6,8 +6,11 @@ function App() {
 
   return (
     <Routes>
+      <Route path='/' element={<Layout></Layout>}>
       <Route index element={<IndexPage></IndexPage>}></Route>
       <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+      </Route>
+      
     </Routes>
   )
 
