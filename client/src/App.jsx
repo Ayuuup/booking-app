e6bb70f4fbd91045386142bc0dd5f040a59af2d9
Ayuuup <babakhouyaayoub@gmail.com>
2025-06-19 13:42:33 +0100

    put the cookie  on the api domain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import RegisterPage from './pages/RegisterPage'
 import {Routes,Route} from 'react-router-dom'
 function App() {
   //define a base uri
-  axios.defaults.baseURL= "http://localhost:4000"
+  axios.defaults.baseURL= "http://127.0.0.1:4000"
+  axios.defaults.withCredentials = true
 
   return (
     <Routes>
